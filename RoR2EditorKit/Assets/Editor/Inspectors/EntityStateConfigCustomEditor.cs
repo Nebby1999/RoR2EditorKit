1825398ec57ed279beb211a1e7d70bfc3ff49b8d
Nebby <nebby131999@gmail.com>
2021-10-27 15:23:45 -0300

    Moved certain editor scripts from MSEU to RoR2EditorKit

---
+++ RoR2EditorKit/Assets/Editor/Inspectors/EntityStateConfigCustomEditor.cs
@@ -0,0 +1,30 @@
+﻿using RoR2;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace RoR2EditorKit
+{
+    [CustomEditor(typeof(EntityStateConfiguration))]
+    public class EntityStateConfigCustomEditor : Editor
+    {
+        [OnOpenAsset]
+        public static bool OpenEditor(int instanceID, int line)
+        {
+            EntityStateConfiguration obj = EditorUtility.InstanceIDToObject(instanceID) as EntityStateConfiguration;
+            if (obj != null)
+            {
+                ExtendedEditorWindow.OpenEditorWindow<EntityStateConfigEditorWindow>(obj, "Entity State Configuration Editor");
+                return true;
+            }
+            return false;
+        }
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Open Editor"))
+            {
+                ExtendedEditorWindow.OpenEditorWindow<EntityStateConfigEditorWindow>(target, "Entity State Configuration Editor");
+            }
+        }
+    }
+}
---
+++ RoR2EditorKit/Assets/Editor/Windows/SerializableContentPackEditorWindow.cs
@@ -0,0 +1,72 @@
+﻿using RoR2.ContentManagement;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace RoR2EditorKit
+{
+    class SerializableContentPackEditorWindow : ExtendedEditorWindow
+    {
+        Vector2 scrollPos = new Vector2();
+        SerializableContentPack contentPack;
+        string selectedArrayPath;
+
+        private void OnGUI()
+        {
+            contentPack = mainSerializedObject.targetObject as SerializableContentPack;
+            string[] fieldNames = contentPack.GetType()
+                .GetFields()
+                .Select(fieldInfo => fieldInfo.Name)
+                .ToArray();
+
+            EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+            EditorGUILayout.BeginVertical("box", GUILayout.MaxWidth(300), GUILayout.ExpandHeight(true));
+
+            DrawButtonSidebar(fieldNames);
+
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.BeginVertical("box", GUILayout.ExpandHeight(true));
+            if (mainSelectedProperty != null)
+            {
+                DrawSelectedArray();
+            }
+            else
+            {
+                EditorGUILayout.LabelField("Select an Content Element from the List.");
+            }
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndHorizontal();
+
+            ApplyChanges();
+        }
+
+        private void DrawButtonSidebar(string[] fieldNames)
+        {
+            foreach (string field in fieldNames)
+            {
+                if (GUILayout.Button(field))
+                {
+                    selectedArrayPath = mainSerializedObject.FindProperty(field).propertyPath;
+                }
+            }
+            if (!string.IsNullOrEmpty(selectedArrayPath))
+            {
+                mainSelectedProperty = mainSerializedObject.FindProperty(selectedArrayPath);
+            }
+        }
+
+        private void DrawSelectedArray()
+        {
+            mainCurrentProperty = mainSelectedProperty;
+
+            EditorGUILayout.BeginHorizontal("box");
+            EditorGUILayout.BeginVertical("box", GUILayout.MaxWidth(500));
+
+            DrawValueSidebar(mainCurrentProperty);
+
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
