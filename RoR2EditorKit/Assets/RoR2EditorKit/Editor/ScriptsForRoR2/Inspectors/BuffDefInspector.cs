0cc163abb196808cd70c0b8baf3a6ccea4d47866
Nebby <78170922+Nebby1999@users.noreply.github.com>
2022-03-02 21:46:08 -0300

    Added buffDef inbspector

--- RoR2EditorKit/Assets/RoR2EditorKit/Editor/ScriptsForRoR2/Inspectors/BuffDefInspector.cs
+++ RoR2EditorKit/Assets/RoR2EditorKit/Editor/ScriptsForRoR2/Inspectors/BuffDefInspector.cs
@@ -17,21 +17,42 @@ namespace RoR2EditorKit.RoR2Related.Inspectors
     {
         protected override VisualElement DrawInspectorGUI()
         {
+            VisualElement element = new VisualElement();
+
             var header = RootVisualElement.Q<VisualElement>("Header");
+            element.Add(header);
 
-            var buffName = RootVisualElement.Query().Descendents<Label>("BuffDefName").First();
+            var buffName = header.Q<Label>("buffDefName");
+            buffName.BindProperty(serializedObject.FindProperty("m_Name"));
 
-            /*var iconSprite = QuickQ<ObjectField>("IconSprite");
-            var buffColor = QuickQ<ColorField>("BuffColor");
-            var eliteDef = QuickQ<ObjectField>("EliteDef");
-            var startSFX = QuickQ<ObjectField>("StartSFX");
-            var canStack = QuickQ<Toggle>("canStack");
-            var isDebuff = QuickQ<Toggle>("IsDebuff");
-            var isCooldown = QuickQ<Toggle>("IsCooldown");
-            var isHidden = QuickQ<Toggle>("IsHidden");*/
+            var inspectorData = RootVisualElement.Q<VisualElement>("InspectorData");
+            element.Add(inspectorData);
 
-            //buffName.text = targetType.name;
-            return new Label("Lol");
+            var iconSprite = inspectorData.Q<ObjectField>("IconSprite");
+            iconSprite.objectType = typeof(Sprite);
+            iconSprite.BindProperty(serializedObject.FindProperty("iconSprite"));
+
+            var buffColor = inspectorData.Q<ColorField>("BuffColor");
+            buffColor.BindProperty(serializedObject.FindProperty("buffColor"));
+
+            var eliteDef = inspectorData.Q<ObjectField>("EliteDef");
+            eliteDef.objectType = typeof(EliteDef);
+            eliteDef.BindProperty(serializedObject.FindProperty("eliteDef"));
+
+            var startSFX = inspectorData.Q<ObjectField>("StartSFX");
+            startSFX.objectType = typeof(NetworkSoundEventDef);
+            startSFX.BindProperty(serializedObject.FindProperty("startSfx"));
+
+            var canStack = inspectorData.Q<Toggle>("CanStack");
+            canStack.BindProperty(serializedObject.FindProperty("canStack"));
+
+            var isDebuff = inspectorData.Q<Toggle>("IsDebuff");
+            canStack.BindProperty(serializedObject.FindProperty("isCooldown"));
+
+            var isHidden = inspectorData.Q<Toggle>("IsHidden");
+            canStack.BindProperty(serializedObject.FindProperty("isHidden"));
+
+            return element;
         }
     }
 }
