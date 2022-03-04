4045e9772cc40090898801bef36f77d1a0aa27b9
Nebby <78170922+Nebby1999@users.noreply.github.com>
2022-03-04 12:27:24 -0300

    Pipeline that replaces text with new text.

--- RoR2EditorKit/Assets/RoR2EditorKit/Editor/Core/PipelineJobs/ReplaceTextInFiles.cs
+++ RoR2EditorKit/Assets/RoR2EditorKit/Editor/Core/PipelineJobs/ReplaceTextInFiles.cs
@@ -1,73 +1,75 @@
-using ThunderKit.Core.Pipelines;
-using ThunderKit.Core.Attributes;
-using ThunderKit.Core.Manifests.Datum;
 using RoR2EditorKit.Core.ManifestDatums;
-using UnityEngine;
+using System.Threading.Tasks;
+using ThunderKit.Core.Pipelines;
 using UnityEditor;
-using System.Linq;
+using UnityEngine;
+using ThunderKit.Core.Attributes;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 using ThunderKit.Core.Data;
-using System.Threading.Tasks;
-using System.Text;
 
 namespace RoR2EditorKit.Core.PipelineJobs
 {
-    [PipelineSupport(typeof(Pipeline)), RequiresManifestDatumType(typeof(SetObjectFlagsDatum))]
-    public class SetObjectFlags : PipelineJob
+    [PipelineSupport(typeof(Pipeline)), RequiresManifestDatumType(typeof(ReplaceTextInFiles))]
+    public class ReplaceTextInFiles : PipelineJob
     {
-        public bool AffectsDependencies = false;
-        public HideFlags flags = HideFlags.None;
+        public string textToReplace;
+        public string replacementText;
         public override Task Execute(Pipeline pipeline)
         {
             AssetDatabase.SaveAssets();
 
             var manifests = pipeline.Manifests;
 
-            var files = new List<SetObjectFlagsDatum>();
+            var files = new List<ReplaceTextInFilesDatum>();
             for(int i = 0; i < manifests.Length; i++)
             {
-                foreach(var filesDatum in manifests[i].Data.OfType<SetObjectFlagsDatum>())
+                foreach(var textInFilesDatum in manifests[i].Data.OfType<ReplaceTextInFilesDatum>())
                 {
-                    files.Add(filesDatum);
+                    files.Add(textInFilesDatum);
                 }
             }
 
-            var filesDatums = files.ToArray();
+            var textInFilesDatums = files.ToArray();
             var explicitAssets = files
-                .SelectMany(f => f.objects)
+                .SelectMany(f => f.Objects)
                 .ToArray();
 
             var explicitAssetPaths = new List<string>();
 
             PopulateWithExplicitAssets(explicitAssets, explicitAssetPaths);
 
-
             for(int defIndex = 0; defIndex < files.Count; defIndex++)
             {
-                var fileDatum = filesDatums[defIndex];
+                var textFileDatum = textInFilesDatums[defIndex];
                 var assets = new List<string>();
-                var firstAsset = fileDatum.objects.FirstOrDefault(x => x is SceneAsset);
+                var firstAsset = textFileDatum.Objects.FirstOrDefault(x => x is SceneAsset);
 
                 if (firstAsset != null) assets.Add(AssetDatabase.GetAssetPath(firstAsset));
                 else
                 {
-                    PopulateWithExplicitAssets(fileDatum.objects, assets);
-
-                    if(AffectsDependencies)
-                    {
-                        var dependencies = assets.SelectMany(assetPath => AssetDatabase.GetDependencies(assetPath))
-                            .Where(dep => !explicitAssetPaths.Contains(dep))
-                            .ToArray();
-                        assets.AddRange(dependencies);
-                    }
+                    PopulateWithExplicitAssets(textFileDatum.Objects, assets);
                 }
 
-                foreach (string assetPath in explicitAssetPaths.Where(p => !p.IsNullOrEmptyOrWhitespace()))
+                foreach(string assetPath in explicitAssetPaths.Where(p => !p.IsNullOrEmptyOrWhitespace()))
                 {
-                    Object asset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
-                    pipeline.Log(LogLevel.Information, $"Changing {asset}'s hideFlags ({asset.hideFlags}) to {flags}");
-                    asset.hideFlags = flags;
+                    string[] text = File.ReadAllLines(assetPath);
+                    List<string> newText = new List<string>();
+
+                    for(int i = 0; i < text.Length; i++)
+                    {
+                        if(text[i].Contains(textToReplace))
+                        {
+                            pipeline.Log(LogLevel.Information, $"Replacing {textToReplace} (Line {i}) with {replacementText} {AssetDatabase.LoadAssetAtPath<Object>(assetPath)}");
+                            newText.Add(text[i].Replace(textToReplace, replacementText));
+                        }
+                        else
+                        {
+                            newText.Add(text[i]);
+                        }
+                    }
+                    File.WriteAllLines(assetPath, newText.ToArray());
                 }
             }
 
